ee95baefa24d83aadc0b147b4f614c1621e19f28
Ambi <ambi92@outlook.com>
2020-04-24 21:58:25 -0400

    Implemented Butterknife to Activity Detail.

--- app/src/main/java/com/example/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/popularmovies/DetailActivity.java
@@ -11,15 +11,23 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
     //Link for server to get Image.
     private static final String BASE_URL_IMAGE = "http://image.tmdb.org/t/p/w500";
     //Giving name for ImageView and TextView
+    @BindView(R.id.moviePoster_IV)
     ImageView poster_IV;
+    @BindView(R.id.movieTitle_TV)
     TextView title_TV;
+    @BindView(R.id.moviePlot_TV)
     TextView plot_TV;
+    @BindView(R.id.movieReleasedate_TV)
     TextView releasedate_TV;
+    @BindView(R.id.movieRating_TV)
     TextView rating_TV;
 
     //This is to get information from MainActivity using Intents
@@ -35,7 +43,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        ButterKnife.bind(this);
         poster_IV = findViewById(R.id.moviePoster_IV);
         title_TV = findViewById(R.id.movieTitle_TV);
         plot_TV = findViewById(R.id.moviePlot_TV);
